fa6c5c347475a1bf525117da9dc472d048a4b558
oleglokhtin <olohtin@gmail.com>
2025-08-22 12:08:16 +0300

    Likes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
+    const [likes, setLikes] = useState(5);
+
+    function increment() {
+        setLikes(likes + 1);
+        console.log(likes);
+    }
+
+    function decrement() {
+        setLikes(likes - 1);
+        console.log(likes);
+    }
+    return (
     <div className="App">
+        <h1>{likes}</h1>
+        <button onClick={increment}> Increment</button>
+        <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
 
 export default App;
+//npm start
